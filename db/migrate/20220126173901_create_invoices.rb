9496620036e9b138edbd3bcb2f601a57c2dc2784
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2022-01-26 12:39:14 -0500

    creating invoices

---
+++ db/migrate/20220126173901_create_invoices.rb
@@ -0,0 +1,10 @@
+class CreateInvoices < ActiveRecord::Migration[7.0]
+  def change
+    create_table :invoices do |t|
+      t.integer :user_id
+      t.string :number
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/invoices.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :invoice do
+    user_id { 1 }
+    number { "MyString" }
+  end
+end
