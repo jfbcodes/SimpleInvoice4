d6ab2970226cf98fcf470260f76cabf21a481e4b
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2022-01-26 12:39:47 -0500

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :admin_dashboard do
+    # on the admin dashboard I can see & edit all records (god controllers)
+    resources :invoices # admin/invoices_controller.rb
+
+    resources :users do # admin/users_controller.rb
+      resources :invoices # admin/invoices_controller.rb
+    end
+  end
+
+  namespace :user_dashboard do
+    # on the user dashboard I can see & edit only my records
+    resources :users do # users_controller.rb
+      resources :invoices # invoices_controller.rb
+    end
+  end
 end
